680d9da1beeaa0bdd210d1c1f88caad6255ca2ec
Juan Isaza <juanisaza1114966@correo.itm.edu.co>
2026-04-04 20:34:01 -0500

    prueba de commit

---
+++ src/main/java/itm/grupoz/blogprivado/BlogprivadoApplication.java
@@ -0,0 +1,13 @@
+package itm.grupoz.blogprivado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogprivadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogprivadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/itm/grupoz/blogprivado/BlogprivadoApplicationTests.java
@@ -0,0 +1,13 @@
+package itm.grupoz.blogprivado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogprivadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
